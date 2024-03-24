b6a2855b2c1d3fe019ef21146e7805c3c31e9c14
Jacob Mulli <jacobmullie2@gmail.com>
2024-03-24 18:40:00 +0300

    changed the title abit

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -23,9 +23,7 @@ const HeroContent = () => {
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
-          </h1>
+          <h1 className="Welcome-text text-[13px]">Welcome To My Portfolio</h1>
         </motion.div>
 
         <motion.div
@@ -33,10 +31,10 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w--[600px] w-auto h-auto"
         >
           <span>
-            Providing
+            I&apos;m Jacob Mulli
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              the best{" "}
+              Providing the best{" "}
             </span>
             project experience
           </span>
