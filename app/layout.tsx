7c235c481bceb067b1fe6fdca82fe621a6538e01
Jacob Mulli <95289696+mulli-dev@users.noreply.github.com>
2024-06-10 21:22:39 +0300

    added Mulli Portfolio as the header -tab-name

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Portfolio",
+  title: "Mulli Portfolio",
   description: " This is my portfolio",
 };
 
