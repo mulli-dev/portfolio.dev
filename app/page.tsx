1bab84b1e6a61ebd464ec6fd33cffab0d1224bc0
Jacob Mulli <95289696+mulli-dev@users.noreply.github.com>
2024-06-11 10:07:12 +0300

    added widgets

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,20 @@ import Image from "next/image";
 import Skills from "@/components/main/Skills";
 import Encryption from "@/components/main/Encryption";
 import Projects from "@/components/main/Projects";
+import Head from "next/head";
 
 import Hero from "@/components/main/Hero";
 export default function Home() {
   return (
     <main className="h-full w-full">
+      <Head>
+        <script
+          id="pixel-chaty"
+          async
+          src="https://cdn.chaty.app/pixel.js?id=GhXkF5oM"
+        ></script>
+      </Head>
+
       <div className="flex flex-col  gap-20">
         <Hero />
         <Skills />
